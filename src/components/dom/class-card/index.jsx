7c0523aa1b738f19ecf3d2445cf5baab105c5117
Nour Malaeb <nourmalaeb@gmail.com>
2022-12-20 22:35:24 -0800

    Stuff

--- src/components/dom/class-card/index.jsx
+++ src/components/dom/class-card/index.jsx
@@ -1,11 +1,17 @@
 import Image from 'next/image'
+import styles from './styles.module.scss'
+import { syne } from '@/styles/fonts'
 
 const Card = ({ title, content, bgcolor = `#150A33`, pic }) => {
   return (
-    <div style={{ backgroundColor: bgcolor }}>
-      <Image src={pic} alt='' />
-      <h3>{title}</h3>
-      <p>{content}</p>
+    <div className={styles.card}>
+      <div className={styles.cardImage}>
+        <Image src={pic} alt='' fill />
+      </div>
+      <div className={styles.content}>
+        <h3 className={syne.className}>{title}</h3>
+        <p>{content}</p>
+      </div>
     </div>
   )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Header from '../components/dom/header'
 import Image from 'next/image'
 import { unbounded } from '@/styles/fonts'
+import Card from '@/components/dom/class-card'
 
 const Home = () => {
   return (
     <>
       <Header />
       {/* ABOUT SECTION */}
-      <section id='about'>
+      <section id='about' className='simple'>
         <div className='column'>
           <h2 className={`sectionTitle ${unbounded.className}`}>Web3 Garages for Everyone</h2>
           <p>
@@ -18,11 +19,11 @@ const Home = () => {
           </p>
         </div>
         <div className='framed' style={{ aspectRatio: 2, height: 300 }}>
-          <Image src='/img/barry.jpg' alt='' fill />
+          <Image src='/img/barry.jpg' alt='' fill style={{ objectFit: 'cover' }} />
         </div>
       </section>
-      {/* CURRICULUM SECTION */}
-      <section id='curriculum'>
+      {/* COMMUNITIES SECTION */}
+      <section id='communities' className='simple'>
         <div className='column'>
           <h2 className={`sectionTitle ${unbounded.className}`}>Our Communities</h2>
           <p>
@@ -32,7 +33,44 @@ const Home = () => {
           </p>
         </div>
         <div className='framed' style={{ aspectRatio: 2, height: 300 }}>
-          <Image src='/img/barry.jpg' alt='' fill />
+          <Image src='/img/barry.jpg' alt='' fill style={{ objectFit: 'cover' }} />
+        </div>
+      </section>
+      {/* GALLERY SECTION */}
+      <section id='gallery' className='gallery'>
+        <div className='column'>
+          <h2 className={`sectionTitle ${unbounded.className}`}>Gallery</h2>
+          <p>
+            The problems of today’s Internet are due in large part to lack of diversity among its leading creators. It’s
+            critical that Web3 makers include everyone. These creators will shape the Internet and culture for decades
+            to come.
+          </p>
+        </div>
+        <div className='framed' style={{ aspectRatio: 2, height: 300 }}>
+          <Image src='/img/barry.jpg' alt='' fill style={{ objectFit: 'cover' }} />
+        </div>
+      </section>
+      {/* CURRICULUM SECTION */}
+      <section id='curriculum' className='simple'>
+        <div className='column'>
+          <h2 className={`sectionTitle ${unbounded.className}`}>Curriculum</h2>
+          <p>
+            The problems of today’s Internet are due in large part to lack of diversity among its leading creators. It’s
+            critical that Web3 makers include everyone. These creators will shape the Internet and culture for decades
+            to come.
+          </p>
+        </div>
+        <div>
+          <Card
+            title='Introduction to the Metaverse'
+            content='Intro class getting acquainted with terms, technology, and real-world applications of Virtual Reality.'
+            pic='/img/barry.jpg'
+          />
+          <Card
+            title='Introduction to the Metaverse'
+            content='Intro class getting acquainted with terms, technology, and real-world applications of Virtual Reality.'
+            pic='/img/barry.jpg'
+          />
         </div>
       </section>
     </>
