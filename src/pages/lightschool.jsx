1d1ad12c3a7be65502df094f095c346e5ac30815
Nour Malaeb <nourmalaeb@gmail.com>
2023-01-14 23:29:26 -0500

    Adds lightschool page

---
+++ src/pages/lightschool.jsx
@@ -0,0 +1,15 @@
+const Lightschool = () => (
+  <div style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}>
+    <iframe
+      width='100%'
+      height='100%'
+      src='https://ynceoz.gmetri.com/whose_metaverse'
+      id=''
+      frameborder='0'
+      allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; microphone; camera; display-capture;'
+      allowfullscreen
+    ></iframe>
+  </div>
+)
+
+export default Lightschool
