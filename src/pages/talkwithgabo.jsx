c8fe289af03ba3abe6ca181d4de14fc07ae71157
Nour Malaeb <nourmalaeb@gmail.com>
2023-01-25 13:28:19 -0500

    Add talkwithgabo page

---
+++ src/pages/talkwithgabo.jsx
@@ -0,0 +1,14 @@
+const TalkWithGabo = () => (
+  <div style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}>
+    <iframe
+      src='https://ask.storyfile.com/?uid=6409&skin=web&topics=1'
+      width='100%'
+      height='100%'
+      allow='microphone'
+      frameborder='0'
+      style={{ border: 0, minHeight: '80vh' }}
+    ></iframe>
+  </div>
+)
+
+export default TalkWithGabo
